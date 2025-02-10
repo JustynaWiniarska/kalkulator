e25b58f9b75dc04146360dc3b78deb11c784b176
Justyna <justyna.field@gmail.com>
2025-02-10 18:57:58 +0100

    Adds components/ui folder, Input and Label components from shadcn

---
+++ components/ui/Label.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Label, type LabelProps } from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
+
+const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <Label
+    v-bind="delegatedProps"
+    class="text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+    :class="props.class"
+  >
+    <slot />
+  </Label>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,33 @@
 <template>
-  <h2>Here</h2>
+  <div class="max-w-xl m-auto my-20 font-serif tracking-wide">
+    <h1 class="font-bold text-2xl mb-6">Kalkulator OC/AC</h1>
+    <p class="text-lg">Poniższy kalkulator służy do obliczenia rocznej składki ubezpieczenia OC/AC na podstawie wieku samochodu i jego wartości.</p>
+
+      <!-- kalkulator -->
+      <div class="my-10">
+        <div class="mb-4">
+            <Label for="rocznik">Rocznik</Label>
+            <Input id="rocznik" type="text" />
+          </div>
+        <!-- ceny -->
+        <div class="grid grid-cols-2 gap-4">
+          <div class="mb-4">
+            <Label for="cena-netto">Cena netto</Label>
+            <Input id="cena-netto" type="text" />
+          </div>
+          <div class="mb-4">
+            <Label for="cena-brutto">Cena brutto</Label>
+            <Input id="cena-brutto" type="text" />
+          </div>
+        </div>
+      </div>
+
+
+  </div>
 </template>
+
+
+<script setup lang="ts">
+import Label from '@/components/ui/Label.vue'
+import Input from '@/components/ui/Input.vue'
+</script>
